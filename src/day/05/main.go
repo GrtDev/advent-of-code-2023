699f07d8b653ba7ba5db16e6c22ac765d318e33b
Geert Fokke <geert@grt-dev.com>
2023-12-06 21:38:25 +0100

    improve 5B

--- src/day/05/main.go
+++ src/day/05/main.go
@@ -10,15 +10,22 @@ import (
 )
 
 type indexRange struct {
+	start  int
+	end    int
+	length int
+}
+
+type indexRangeMap struct {
 	destination int
-	source      int
+	start       int
+	end         int
 	length      int
 }
 
 type indexMap struct {
 	sourceId      string
 	destinationId string
-	ranges        []indexRange
+	ranges        []indexRangeMap
 }
 
 func getInput(inputFile string) []string {
@@ -48,21 +55,25 @@ func RunB(inputFile string) (int, error) {
 	seeds := parseSeedsString(input[0])
 	maps := parseMaps(input[1:])
 
-	seedsAdjusted := []int{}
+	seedRanges := []indexRange{}
 	for i := 0; i < len(seeds); i += 2 {
 		seed := seeds[i]
-		seedCount := seeds[i+1]
+		length := seeds[i+1]
 
-		for j := 0; j < seedCount; j++ {
-			seedsAdjusted = append(seedsAdjusted, seed+j)
-		}
+		seedRanges = append(seedRanges, indexRange{
+			start:  seed,
+			end:    seed + length - 1,
+			length: length,
+		})
 	}
 
-	seedLocations := funk.Map(seedsAdjusted, func(seed int) int {
-		return transformIndex(seed, "seed", "location", maps)
+	processedRanges := transformIndexRange(seedRanges, "seed", "location", maps)
+
+	lowestSeedLocations := funk.Map(processedRanges, func(iRange indexRange) int {
+		return iRange.start
 	}).([]int)
 
-	return funk.MinInt(seedLocations), nil
+	return funk.MinInt(lowestSeedLocations), nil
 
 }
 
@@ -81,14 +92,15 @@ func parseMaps(input []string) []indexMap {
 		matches := mapRegexp.FindStringSubmatch(paragraph)
 		rangeStrings := utils.DropEmptyStrings(strings.Split(matches[3], "\n"))
 
-		ranges := funk.Map(rangeStrings, func(rangeString string) indexRange {
+		ranges := funk.Map(rangeStrings, func(rangeString string) indexRangeMap {
 			rangeNumbers, _ := utils.StringToInts(rangeString)
-			return indexRange{
+			return indexRangeMap{
 				destination: rangeNumbers[0],
-				source:      rangeNumbers[1],
+				start:       rangeNumbers[1],
 				length:      rangeNumbers[2],
+				end:         rangeNumbers[1] + rangeNumbers[2] - 1,
 			}
-		}).([]indexRange)
+		}).([]indexRangeMap)
 
 		maps = append(maps, indexMap{
 			sourceId:      matches[1],
@@ -102,8 +114,8 @@ func parseMaps(input []string) []indexMap {
 
 func calculateIndex(iMap indexMap, sourceIndex int) int {
 	for _, indexRange := range iMap.ranges {
-		if sourceIndex >= indexRange.source && sourceIndex <= indexRange.source+indexRange.length {
-			return indexRange.destination + (sourceIndex - indexRange.source)
+		if sourceIndex >= indexRange.start && sourceIndex <= indexRange.start+indexRange.length {
+			return indexRange.destination + (sourceIndex - indexRange.start)
 		}
 	}
 	return sourceIndex
@@ -116,6 +128,98 @@ func transformIndex(index int, from string, to string, maps []indexMap) int {
 		return index
 	}
 	return transformIndex(index, iMap.destinationId, to, maps)
+}
+
+func calculateIndexRanges(input []indexRange, iMap indexMap) []indexRange {
+	ranges := []indexRange{}
+	for _, iRange := range input {
+		ranges = append(ranges, processMaps(iRange, iMap.ranges)...)
+	}
+
+	return funk.Filter(ranges, func(iRange indexRange) bool {
+		return iRange.length > 0
+	}).([]indexRange)
+}
+
+func processMaps(input indexRange, rangeMaps []indexRangeMap) []indexRange {
+	results := []indexRange{}
+	lenMaps := len(rangeMaps)
+
+	rangeMap := rangeMaps[0]
+	before, after, processed := transformRange(input, rangeMap)
+	results = append(results, processed)
+
+	if before.length > 0 {
+		if lenMaps > 1 {
+			results = append(results, processMaps(before, rangeMaps[1:])...)
+		} else {
+			results = append(results, before)
+		}
+	}
+	if after.length > 0 {
+		if lenMaps > 1 {
+			results = append(results, processMaps(after, rangeMaps[1:])...)
+		} else {
+			results = append(results, after)
+		}
+	}
+	return results
+}
+
+func transformRange(i indexRange, mapRange indexRangeMap) (indexRange, indexRange, indexRange) {
+
+	if i.end < mapRange.start {
+		return i, indexRange{}, indexRange{}
+	}
+	if i.start > mapRange.end {
+		return indexRange{}, i, indexRange{}
+	}
+
+	iBefore := indexRange{}
+	iAfter := indexRange{}
+
+	if i.start < mapRange.start {
+		iBefore = indexRange{
+			start:  i.start,
+			end:    mapRange.start - 1,
+			length: mapRange.start - i.start,
+		}
+		i.start = mapRange.start
+	}
+
+	if i.end > mapRange.end {
+		iAfter = indexRange{
+			start:  mapRange.end + 1,
+			end:    i.end,
+			length: i.end - mapRange.end,
+		}
+		i.end = mapRange.end
+	}
+
+	i.length = i.end - i.start
+
+	processStart := funk.MaxInt([]int{i.start, mapRange.start})
+	processEnd := funk.MinInt([]int{i.end, mapRange.end})
+	delta := mapRange.destination - mapRange.start
+
+	iProcessed := indexRange{
+		start:  delta + processStart,
+		end:    delta + processEnd,
+		length: processEnd - processStart + 1,
+	}
+
+	return iBefore, iAfter, iProcessed
+}
+
+func transformIndexRange(i []indexRange, from string, to string, maps []indexMap) []indexRange {
+	iMap := getIndexMap(from, maps)
+	i = calculateIndexRanges(i, iMap)
+
+	if iMap.destinationId == to {
+		return i
+	}
+
+	return transformIndexRange(i, iMap.destinationId, to, maps)
 }
 
 func getIndexMap(sourceId string, maps []indexMap) indexMap {
--- src/day/05/main_test.go
+++ src/day/05/main_test.go
@@ -29,3 +29,100 @@ func TestB(t *testing.T) {
 		t.Fatalf(`expected: %v, to equal: %v`, result, expected)
 	}
 }
+func TestTransformRange(t *testing.T) {
+	source := []indexRange{
+		{start: 10, end: 20, length: 11},
+	}
+
+	iMap := indexMap{
+		sourceId:      "source",
+		destinationId: "destination",
+		ranges: []indexRangeMap{
+			{destination: 0, start: 15, end: 20, length: 6},
+			{destination: 0, start: 30, end: 35, length: 6},
+		},
+	}
+
+	result := calculateIndexRanges(source, iMap)
+
+	expected := []indexRange{
+		{start: 0, end: 5, length: 6},
+		{start: 10, end: 14, length: 5},
+	}
+
+	if len(result) != len(expected) {
+		t.Fatalf(`expected: length %v, to equal: %v`, len(result), len(expected))
+	}
+	if result[0] != expected[0] {
+		t.Fatalf(`expected: %v, to equal: %v`, result[0], expected[0])
+	}
+	if result[1] != expected[1] {
+		t.Fatalf(`expected: %v, to equal: %v`, result[0], expected[0])
+	}
+}
+
+func TestTransformRange2(t *testing.T) {
+	source := []indexRange{
+		{start: 10, end: 20, length: 11},
+		{start: 33, end: 34, length: 2},
+	}
+
+	iMap := indexMap{
+		sourceId:      "source",
+		destinationId: "destination",
+		ranges: []indexRangeMap{
+			{destination: 0, start: 15, end: 20, length: 6},
+			{destination: 0, start: 30, end: 35, length: 6},
+		},
+	}
+
+	result := calculateIndexRanges(source, iMap)
+
+	expected := []indexRange{
+		{start: 0, end: 5, length: 6},
+		{start: 10, end: 14, length: 5},
+		{start: 3, end: 4, length: 2},
+	}
+
+	if len(result) != len(expected) {
+		t.Fatalf(`expected: length %v, to equal: %v`, len(result), len(expected))
+	}
+	if result[0] != expected[0] {
+		t.Fatalf(`expected: %v, to equal: %v`, result[0], expected[0])
+	}
+	if result[1] != expected[1] {
+		t.Fatalf(`expected: %v, to equal: %v`, result[0], expected[0])
+	}
+	if result[2] != expected[2] {
+		t.Fatalf(`expected: %v, to equal: %v`, result[0], expected[0])
+	}
+}
+
+func TestTransformRange3(t *testing.T) {
+	source := []indexRange{
+		{start: 33, end: 38, length: 6},
+	}
+
+	iMap := indexMap{
+		sourceId:      "source",
+		destinationId: "destination",
+		ranges: []indexRangeMap{
+			{destination: 0, start: 15, end: 20, length: 6},
+			{destination: 0, start: 30, end: 35, length: 6},
+		},
+	}
+
+	result := calculateIndexRanges(source, iMap)
+
+	expected := []indexRange{
+		{start: 3, end: 5, length: 3},
+		{start: 36, end: 38, length: 3},
+	}
+
+	if len(result) != len(expected) {
+		t.Fatalf(`expected: length %v, to equal: %v`, len(result), len(expected))
+	}
+	if result[0] != expected[0] {
+		t.Fatalf(`expected: %v, to equal: %v`, result[0], expected[0])
+	}
+}
